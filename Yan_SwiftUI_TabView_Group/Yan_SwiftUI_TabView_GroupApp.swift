d59919741547ec41d941b16fb49f89e85f255711
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 14:02:44 +0500

    Initial Commit

---
+++ Yan_SwiftUI_TabView_Group/Yan_SwiftUI_TabView_GroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_TabView_GroupApp.swift
+//  Yan_SwiftUI_TabView_Group
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_TabView_GroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
